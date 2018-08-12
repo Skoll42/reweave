536ee0de3a622325473336e0596a59d2be402bef
Skoll <akostko@gmail.com>
2018-08-12 20:56:00 +0300

    who we are mobile

--- wp-content/themes/common/themplate-who.php
+++ wp-content/themes/common/themplate-who.php
@@ -27,7 +27,7 @@ Template Name: Who we are Page
         <?php
         $circles = get_field('who_circles');
         if (!empty($circles)) : ?>
-            <div class="experts">
+            <div class="experts hidden-xs">
                 <?php $curr = 0; foreach($circles as $circle) : $curr++; ?>
                     <div class="quarter-block">
                         <div class="image">
@@ -52,6 +52,34 @@ Template Name: Who we are Page
                 <?php endforeach;?>
             </div>
         <?php endif; ?>
+    <?php
+    $circles = get_field('who_circles');
+    if (!empty($circles)) : ?>
+        <div class="experts visible-xs">
+            <?php $curr = 0; foreach($circles as $circle) : $curr++; ?>
+                <div class="quarter-block">
+                    <div class="image">
+                        <img src="<?php echo get_stylesheet_directory_uri()?>/img/circle.svg" />
+                        <?php if($circle['who_text_in_cirlcle']): ?>
+                            <div class="bg-over">
+                                <div class="bg-text"><?php echo $circle['who_text_in_cirlcle'];?></div>
+                            </div>
+                        <?php endif; ?>
+                        <?php if($circle['who_bg_image']): ?>
+                            <div class="bg-image"><img src="<?php echo $circle['who_bg_image']?>" /></div>
+                        <?php endif; ?>
+                    </div>
+                    <div class="text">
+                        <div class="title"><?php echo $circle['who_circle_block_title'];?></div>
+                        <?php echo $circle['who_circle_block_description'];?>
+                    </div>
+                </div>
+                <?php if($curr % 2 == 0): ?>
+                    <div class="clearfix"></div>
+                <?php endif; ?>
+            <?php endforeach;?>
+        </div>
+    <?php endif; ?>
     <div class="lines">
         <div class="image-wrapper">
             <img src="<?php echo get_stylesheet_directory_uri()?>/img/lines_page_4.svg" />
