c994760fadf97dbecfaafaff64cc6b851b018939
Skoll <akostko@gmail.com>
2018-08-12 21:21:00 +0300

    fixes for header and our methodology

--- wp-content/themes/common/template-methodology.php
+++ wp-content/themes/common/template-methodology.php
@@ -9,7 +9,7 @@ Template Name: Methodology Page
     <div class="h1-wrapper">
         <h1>OUR METHODOLOGY</h1>
     </div>
-    <div class="wrapper">
+    <div class="wrapper hidden-xs">
         <div class="left-block">
             <div class="top">
                 <div class="half-block">
@@ -48,6 +48,35 @@ Template Name: Methodology Page
             </div>
         </div>
         <div class="clearfix"></div>
+    </div>
+    <div class="wrapper-mobile visible-xs">
+        <div class="mobile-block">
+            <div class="title">
+                <h1>BUILDING <br />VENTURES</h1>
+            </div>
+            <div class="text">
+                <?php the_field('methodology_bulding_ventures_text'); ?>
+            </div>
+        </div>
+        <div class="mobile-block">
+            <div class="title">
+                <h1>IMPACT <br />INVESTMENT FUND</h1>
+            </div>
+            <div class="text">
+                <?php the_field('methodology_impact_investment_fund_text'); ?>
+            </div>
+        </div>
+        <div class="mobile-block">
+            <div class="title">
+                <h1>SYSTEM <br />CHANGE</h1>
+            </div>
+            <div class="text">
+                <?php the_field('methodology_system_change_text'); ?>
+            </div>
+        </div>
+        <div class="flower">
+            <img src="<?php echo get_stylesheet_directory_uri()?>/img/methodology.svg" />
+        </div>
     </div>
 </div>
 <?php get_footer(); ?>
