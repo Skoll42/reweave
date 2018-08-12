9efa668060d9220f24c114fc0229f8da63955d8b
Skoll <akostko@gmail.com>
2018-08-12 19:39:43 +0300

    mobile header fixes

--- wp-content/themes/common/footer.php
+++ wp-content/themes/common/footer.php
@@ -2,9 +2,7 @@
         src="https://code.jquery.com/jquery-3.3.1.min.js"
         integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
         crossorigin="anonymous"></script>
-<!--script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.min.js"></script-->
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.bundle.min.js"></script>
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/script.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.min.js"></script>
 <?php wp_footer(); ?>
 
 </body>
--- wp-content/themes/common/header.php
+++ wp-content/themes/common/header.php
@@ -11,26 +11,28 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> data-spy="scroll" data-target="#navbar" data-offset="30">
-
-<div class="header">
-    <div class="nav-menu fixed-top">
-        <div class="col-xs-12">
-            <nav class="navbar navbar-dark navbar-expand-lg">
+    <div class="header">
+        <nav class="navbar navbar-fixed-top">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
                 <a class="navbar-brand" title="Reweave" href="<?php echo home_url(); ?>">
                     <img src="<?php echo get_stylesheet_directory_uri();?>/img/logo.svg" />
                 </a>
-                <div class="collapse navbar-collapse" id="navbar">
-                    <?php wp_nav_menu( array(
-                        'walker' => new Reweave_Walker_Nav_Menu,
-                        'theme_location' => 'header-menu',
-                        'menu_class' => 'navbar-nav ml-auto',
-                        'container' => false,
-                    ) ); ?>
-                </div>
-            </nav>
-        </div>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <?php wp_nav_menu( array(
+                            'walker' => new Reweave_Walker_Nav_Menu,
+                            'theme_location' => 'header-menu',
+                            'menu_class' => 'nav navbar-nav',
+                            'container' => false,
+                        ) ); ?>
+            </div>
+        </nav>
     </div>
-</div>
-
 
 
